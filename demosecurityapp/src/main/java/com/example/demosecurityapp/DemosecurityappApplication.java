69c4d8469691ac8b38815fdd6aa29f2ff0b7fb41
Gani9999 <eslavathganesh999@example.com>
2026-02-15 12:21:08 +0530

    securityDemo

---
+++ demosecurityapp/src/main/java/com/example/demosecurityapp/DemosecurityappApplication.java
@@ -0,0 +1,15 @@
+package com.example.demosecurityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosecurityappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosecurityappApplication.class, args);
+	}
+
+
+
+}
---
+++ demosecurityapp/src/test/java/com/example/demosecurityapp/DemosecurityappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosecurityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosecurityappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
